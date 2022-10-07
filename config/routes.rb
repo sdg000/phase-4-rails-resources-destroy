ce1f6854a9cb36d3ee686bbbac899fcbf542c0a7
Seth David Gyimah <esec26@gmail.com>
2022-10-07 18:13:30 +0000

    completed code along requirements

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  resources :birds, only: [:index, :show, :create, :update]
+  # resources :birds, only: [:index, :show, :create, :update, :destroy]
+
+  # represent all the 5 RESTFUL actions
+  resources :birds
   patch "/birds/:id/like", to: "birds#increment_likes"
 end
